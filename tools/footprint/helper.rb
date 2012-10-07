9f76a7861aaf2fa33bfd09edae73e7ad3afa1fcb
James Harig <jharig23@gmail.com>
2012-10-07 16:45:02 -0400

    Added element_attr

--- tools/footprint/helper.rb
+++ tools/footprint/helper.rb
@@ -4,4 +4,13 @@ module Helper
     text = text.to_s if text.respond_to?(to_s)
     "\"#{text}\""
   end
-end    
+
+  def element_attr(hash)
+    if hash.respond_to?(:each)
+      hash.each do |key, value|
+        eval "def #{key}; @#{key} || @value; end", value
+        eval "def #{key}=(val); @#{key}=val; end"
+      end
+    end
+  end    
+end
