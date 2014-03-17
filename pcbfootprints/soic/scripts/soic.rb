4f6461a3288af0bf5aa7336371bc2f91c42ab414
James Harig <jharig23@gmail.com>
2014-03-16 20:19:47 -0400

    Soicn-16

--- pcbfootprints/soic/scripts/soic.rb
+++ pcbfootprints/soic/scripts/soic.rb
@@ -9,9 +9,20 @@ attrs = {
     pitch: Unit('1.23 mm'),
     pad_rect: Rectangle.new(width: Unit('6.2 mm')),
     number_of_pads: 8,
-    pad_anchor: :start}
+    pad_anchor: :start
+  },
+  :soic16_narrow => {
+    pad_length: Unit('2.4 mm'),
+    pad_thickness: Unit('0.70 mm'),
+    pitch: Unit('1.27 mm'),
+    pad_rect: Rectangle.new(width: Unit('5.5 mm')),
+    number_of_pads: 16,
+    pad_anchor: :end}
+    
 }
 
 e = Soic.new(attrs[:soic8_narrow])
-
 Renderer.new.render_to_file(e, '../soic8_narrow.fp')
+
+e = Soic.new(attrs[:soic16_narrow])
+Renderer.new.render_to_file(e, '../soic16_narrow.fp')
